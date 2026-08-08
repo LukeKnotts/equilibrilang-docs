4f5df83f5fb3b5275410a2dc7414c9756fe6b3c8
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2026-08-08 14:37:09 -0600

    Equilibrilang work

--- src/pages/docs/basics/Phonemes.vue
+++ src/pages/docs/basics/Phonemes.vue
@@ -4,16 +4,19 @@
   <div class="docs">
     <main>
       <h1>Phonemes</h1>
-      <p>Howdy-do?</p>
-      <h1>Phonotactics:</h1>
-      <iframe
-        src="https://www.geogebra.org/calculator/txxqya4c?embed"
-        width="800"
-        height="600"
-        allowfullscreen
-        style="border: 1px solid #e4e4e4; border-radius: 4px"
-        frameborder="0"
-      ></iframe>
+      <p>
+        The basic building blocks that <i>Equilibrilang</i> uses to build words
+        are phonemes. Usually phonemes are sounds, but in this language they are
+        head positions. In that sense maybe they should be called tilt-emes
+        instead.
+      </p>
+      <p><i>Equilibrilang</i> has 15 phonemes:</p>
+      <p>TODO: [Insert Pictures of Phonemes]</p>
+      <p>
+        A word will be constructed from multiple of these head positions used in
+        a row. However, some head positions, when used directly after one
+        another, could create confusion. Imagine you went from
+      </p>
       <hr />
       <p>
         Next:
--- src/pages/docs/references/RefPhonemes.vue
+++ src/pages/docs/references/RefPhonemes.vue
@@ -5,6 +5,15 @@
     <main>
       <h1>Phonemes (Reference List)</h1>
       <p>General list of all phonemes</p>
+      <h1>3d Visualization of the Phonotactics</h1>
+      <iframe
+        src="https://www.geogebra.org/calculator/txxqya4c?embed"
+        width="800"
+        height="600"
+        allowfullscreen
+        style="border: 1px solid #e4e4e4; border-radius: 4px"
+        frameborder="0"
+      ></iframe>
       <hr />
       <p>Next: <router-link to="/">Home Page</router-link></p>
     </main>
